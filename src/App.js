542c7f3a7aed36fde586db3a4edeb681a7f940d2
VirginiaKMartinez <hola@virginiak.es>
2023-02-03 15:01:58 +0100

    css adjustments

--- src/App.js
+++ src/App.js
@@ -263,7 +263,7 @@ function FactList({ facts, setFacts }) {
   }
 
   return (
-    <section>
+    <section className="facts-container">
       <ul className="facts-list">
         {facts.map((fact) => (
           <Fact key={fact.id} fact={fact} setFacts={setFacts} />
@@ -276,6 +276,8 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -294,6 +296,9 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? (
+          <span className="disputed"> [⛔️ DISPUTED ⛔️] </span>
+        ) : null}
         {fact.text}
         <a
           className="source"
